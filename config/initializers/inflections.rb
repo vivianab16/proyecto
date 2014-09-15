858bd575f3635c4202a9e6ca1dad396382f2010b
vivianab16 <vivianabotero9116@gmail.com>
2014-09-15 18:40:34 -0500

    creacion scaffold funcionarios e instructores , migracion de ellas, modificacion de las vistas, controladores y modelos mas una infleccion

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -8,6 +8,7 @@ ActiveSupport::Inflector.inflections(:en) do |inflect|
    inflect.singular /^(ox)en/i, '\1'
    inflect.irregular 'Aprendiz', 'Aprendices'
    inflect.irregular 'Instructor', 'Instructores'
+   inflect.irregular 'Confeccion', 'Confeccion'
 #   inflect.uncountable %w( fish sheep )
  end
 
